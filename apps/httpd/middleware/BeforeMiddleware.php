81fd779524e5306496d16ec7bbea007b4ca2aa71
Rhilip <rhilipruan@gmail.com>
2018-12-06 17:13:55 +0800

    :sparkles: (Middleware) Use BeforeMiddleware to control access

--- apps/httpd/middleware/BeforeMiddleware.php
+++ apps/httpd/middleware/BeforeMiddleware.php
@@ -2,6 +2,10 @@
 
 namespace apps\httpd\middleware;
 
+use mix\facades\PDO;
+use mix\facades\Request;
+use mix\facades\Token;
+
 /**
  * 前置中间件
  * @author 刘健 <coder.liu@qq.com>
@@ -12,8 +16,27 @@ class BeforeMiddleware
     public function handle($callable, \Closure $next)
     {
         // 添加中间件执行代码
+        $userInfo = Token::get('userInfo');
         list($controller, $action) = $callable;
-        // ...
+        $controllerName = get_class($controller);
+
+        if ($controllerName === "apps\httpd\controllers\TrackerController") {
+            return $next();
+        } elseif ($controllerName === "apps\httpd\controllers\AuthController") {
+            if ($action !== "actionLogout")
+                return $next();  // FIXME 更细粒度的控制
+        }
+
+        // For other visit , deny no access_token for other controller fetch
+        if (empty($userInfo)) {
+            return ['errcode' => 300000, 'errmsg' => 'Permission denied'];
+        }
+
+        // Update user status
+        PDO::createCommand("UPDATE `users` SET last_access_at = NOW(), last_access_ip = INET6_ATON(:ip) WHERE id = :id")->bindParams([
+            "ip" => Request::getClientIp(), "id" => $userInfo["uid"]
+        ])->execute();
+
         // 执行下一个中间件
         return $next();
     }
