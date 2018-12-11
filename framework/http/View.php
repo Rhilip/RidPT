1cb5d0d127552fbd9196172dc72b04534b6ce4fb
Rhilip <rhilipruan@gmail.com>
2018-12-11 20:47:36 +0800

    :heavy_plus_sign: (Twig) Add `twig/extensions`

--- framework/http/View.php
+++ framework/http/View.php
@@ -2,8 +2,15 @@
 
 namespace mix\http;
 
-use Twig_Environment;
 use Twig_Loader_Filesystem;
+use Twig_Environment;
+
+use Twig_Extensions_Extension_Array;
+use Twig_Extensions_Extension_Date;
+use Twig_Extensions_Extension_I18n;
+use Twig_Extensions_Extension_Intl;
+use Twig_Extensions_Extension_Text;
+
 
 /**
  * Class View
@@ -11,16 +18,28 @@ use Twig_Loader_Filesystem;
  */
 class View
 {
-    public function render($__template__, $__data__)
+
+    public $twig;
+
+    public function __construct()
     {
         $loader = new Twig_Loader_Filesystem(\Mix::app()->getViewPath());
-        $twig = new Twig_Environment($loader, array(
+        $this->twig = new Twig_Environment($loader, array(
             'debug' => env("APP_DEBUG"),
             'cache' => \Mix::app()->getRuntimePath() . DIRECTORY_SEPARATOR . "view",
         ));
 
+        $this->twig->addExtension(new Twig_Extensions_Extension_Text());
+        $this->twig->addExtension(new Twig_Extensions_Extension_I18n());
+        $this->twig->addExtension(new Twig_Extensions_Extension_Intl());
+        $this->twig->addExtension(new Twig_Extensions_Extension_Array());
+        $this->twig->addExtension(new Twig_Extensions_Extension_Date());
+    }
+
+    public function render($__template__, $__data__)
+    {
         ob_start();
-        echo $twig->render($__template__, $__data__);
+        echo $this->twig->render($__template__, $__data__);
         return ob_get_clean();
     }
 }
