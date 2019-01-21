de7a58a0de9e22b68f0544134813cb31149a7da4
Rhilip <rhilipruan@gmail.com>
2019-01-21 21:30:58 +0800

    :sparkles: (Torrent) Add TorrentFileTree function

--- apps/models/Torrent.php
+++ apps/models/Torrent.php
@@ -34,6 +34,9 @@ class Torrent
     private $torrent_type;
     private $torrent_size;
 
+    const TORRENT_TYPE_SINGLE = 'single';
+    const TORRENT_TYPE_MULTI = 'multi';
+
     public function __construct($id = null)
     {
         $fetch = app()->pdo->createCommand("SELECT * FROM `torrents` WHERE id=:id LIMIT 1;")->bindParams([
@@ -182,13 +185,27 @@ class Torrent
      *
      * [
      *    ["filename" => "f1/f2.text" , "size" => 1234],
-     *    ["filename" => "f1/f3.text" , "size" => 1234],
+     *    ["filename" => "f1/f3.text" , "size" => 2234],
      * ]
      *
+     * elseif $type is "tree" , the return array is like this by using the private static function getFileTree($array, $delimiter = '/')
+     * [
+     *    "f1" => [
+     *        "f2.text" => 1234,
+     *        "f3.text" => 2234
+     *     ]
+     * ]
+     *
+     * Each result will be cached since it will never change.
+     *
+     * @param string $type enum("list","tree") The format of fileList
      * @return array|bool|string
      */
-    public function getFileList()
+    public function getFileList($type = "list")
     {
+        if (!in_array($type, ["list", "tree"]))
+            $type = "list";
+
         $list = app()->redis->get("TORRENT:" . $this->id . ":file_list");
         if ($list === false) {
             $list = app()->pdo->createCommand("SELECT `filename`,`size` FROM `files` WHERE `torrent_id` = :tid ORDER BY `filename` ASC;")->bindParams([
@@ -196,7 +213,64 @@ class Torrent
             ])->queryAll();
             app()->redis->set("TORRENT:" . $this->id . ":file_list", $list);
         }
+
+        if ($type == "tree") {
+            $tree = app()->redis->get("TORRENT:" . $this->id . ":file_list_tree");
+            if ($tree === false) {
+                /**
+                 * Sort $list to this sample by using array_column()
+                 * So that getFileTree() can use this Array()
+                 *
+                 * [
+                 *    "f1/f2.text" => 1234,
+                 *    "f1/f3.text" => 2234
+                 * ]
+                 */
+                $list = array_column($list, 'size', 'filename');
+
+                // Only when Torrent Type is "multi" , We need to use `getFileTree` to convert file list to tree
+                if ($this->getTorrentType() == self::TORRENT_TYPE_MULTI) {
+                    $tree = self::getFileTree($list);
+                    $tree = [$this->getTorrentName() => $tree];
+                }
+
+                app()->redis->set("TORRENT:" . $this->id . ":file_list_tree", $tree);
+            }
+            return $tree;
+        }
+
         return $list;
+    }
+
+    private static function getFileTree($array, $delimiter = '/')
+    {
+        if (!is_array($array)) return array();
+
+        $splitRE = '/' . preg_quote($delimiter, '/') . '/';
+        $returnArr = array();
+        foreach ($array as $key => $val) {
+            // Get parent parts and the current leaf
+            $parts = preg_split($splitRE, $key, -1, PREG_SPLIT_NO_EMPTY);
+            $leafPart = array_pop($parts);
+
+            // Build parent structure
+            // Might be slow for really deep and large structures
+            $parentArr = &$returnArr;
+            foreach ($parts as $part) {
+                if (!isset($parentArr[$part])) {
+                    $parentArr[$part] = array();
+                } elseif (!is_array($parentArr[$part])) {
+                    $parentArr[$part] = array();
+                }
+                $parentArr = &$parentArr[$part];
+            }
+
+            // Add the final part to the structure
+            if (empty($parentArr[$leafPart])) {
+                $parentArr[$leafPart] = $val;
+            }
+        }
+        return $returnArr;
     }
 
     /**
