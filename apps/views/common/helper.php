81c8aff7bfb6b8731153a84cea93533910c249da
Rhilip <rhilipruan@gmail.com>
2019-03-17 23:19:45 +0800

    fix(View): Fix view function redeclare

--- apps/views/common/helper.php
+++ apps/views/common/helper.php
@@ -10,29 +10,33 @@
  *
  */
 
-/**
- * @param \apps\models\Torrent $torrent
- * @return string
- */
-function get_torrent_uploader_id(\apps\models\Torrent $torrent)
-{
-    if ($torrent->getUplver() == 'yes' and app()->user->getClass(true) < app()->config->get('authority.see_anonymous_uploader')) {
-        return 0;
-    } else {
-        return $torrent->getOwnerId();
+if (!function_exists('get_torrent_uploader_id')) {
+    /**
+     * @param \apps\models\Torrent $torrent
+     * @return string
+     */
+    function get_torrent_uploader_id(\apps\models\Torrent $torrent)
+    {
+        if ($torrent->getUplver() == 'yes' and app()->user->getClass(true) < app()->config->get('authority.see_anonymous_uploader')) {
+            return 0;
+        } else {
+            return $torrent->getOwnerId();
+        }
     }
 }
 
-/**
- * @param \apps\models\Torrent $torrent
- * @return string
- */
-function get_torrent_uploader(\apps\models\Torrent $torrent)
-{
-    $owner_id = get_torrent_uploader_id($torrent);
-    if ($owner_id == 0) {
-        return '<i>Anonymous</i>';
-    } else {
-        return "<a class=\"text-default\" href=\"/user/panel?id={$torrent->getOwnerId()}\" data-toggle=\"tooltip\" title=\"User\">{$torrent->getOwner()->getUsername()}</a>";
+if (!function_exists('get_torrent_uploader')) {
+    /**
+     * @param \apps\models\Torrent $torrent
+     * @return string
+     */
+    function get_torrent_uploader(\apps\models\Torrent $torrent)
+    {
+        $owner_id = get_torrent_uploader_id($torrent);
+        if ($owner_id == 0) {
+            return '<i>Anonymous</i>';
+        } else {
+            return "<a class=\"text-default\" href=\"/user/panel?id={$torrent->getOwnerId()}\" data-toggle=\"tooltip\" title=\"User\">{$torrent->getOwner()->getUsername()}</a>";
+        }
     }
 }
